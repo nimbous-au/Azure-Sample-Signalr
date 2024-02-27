213a163a20ea2f60f591c8d7c8db2e9c6206485d
Vishal Patel <vishal.patel@nimbous.com.au>
2024-02-27 13:11:52 +1000

    update to demonstrate how we can use signalr to show onlineusers

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Hosting;
+
+namespace IsolatedModel_BidirectionChat
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
